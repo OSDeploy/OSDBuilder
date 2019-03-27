7a586d6c9c5146cf323620a75396df5c5d055092
David Segura <david@segura.org>
2019-03-26 23:47:34 -0500

    19.3.26.2

--- OSDBuilder.psm1
+++ OSDBuilder.psm1
@@ -1,3 +1,7 @@
+#===================================================================================================
+#   Import Functions
+#   https://github.com/RamblingCookieMonster/PSStackExchange/blob/master/PSStackExchange/PSStackExchange.psm1
+#===================================================================================================
 $OSDPublicFunctions  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $OSDPrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
@@ -7,9 +11,6 @@ foreach ($Import in @($OSDPublicFunctions + $OSDPrivateFunctions)) {
 }
 
 Export-ModuleMember -Function $OSDPublicFunctions.BaseName
-
-#https://github.com/RamblingCookieMonster/PSStackExchange/blob/master/PSStackExchange/PSStackExchange.psm1
-
 #===================================================================================================
 #   Get-OSBuilder
 #===================================================================================================
