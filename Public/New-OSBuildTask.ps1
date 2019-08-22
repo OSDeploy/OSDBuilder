3ed16bccff7785adb81b5897fb0f691857bf17ce
David Segura <david@segura.org>
2019-08-22 11:48:35 -0500

    19.8.22.1

--- Public/New-OSBuildTask.ps1
+++ Public/New-OSBuildTask.ps1
@@ -262,7 +262,7 @@ function New-OSBuildTask {
         Write-Host "-SetUserLocale:                 $SetUserLocale"
         Write-Host "-WinPEAutoExtraFiles:           $WinPEAutoExtraFiles"
         #===================================================================================================
-        Write-Verbose '19.8.21 Get-OSMedia'
+        Write-Verbose '19.8.22 Get-OSMedia'
         #===================================================================================================
         if (!$OSMedia) {
             $OSMedia = @()
@@ -283,7 +283,7 @@ function New-OSBuildTask {
                 Write-Warning "Source OSMedia was not selected . . . Exiting!"
                 Break
             }
-        } elseif ($OSMedia.MediaType -ne 'OSMedia') {
+        } elseif ($OSMedia.MediaType -ne 'OSImport' -or $OSMedia.MediaType -ne 'OSMedia') {
             Write-Warning "Source OSMedia media type is not correct . . . Exiting!"
             Break   
         }
