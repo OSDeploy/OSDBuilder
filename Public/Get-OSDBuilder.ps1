bbd9bf6cd07e655007b16b03a4ce41b3d0aa3147
David Segura <david@segura.org>
2019-09-20 14:11:00 -0500

    19.9.20

--- Public/Get-OSDBuilder.ps1
+++ Public/Get-OSDBuilder.ps1
@@ -295,29 +295,28 @@ function Get-OSDBuilder {
     if ($HideDetails -eq $false) {
         Write-Host ''
 
-        Write-Host 'Change OSDBuilder Home Path:                            ' -NoNewline
+        Write-Host 'Change OSDBuilder Home Path:                        ' -NoNewline
         Write-Host 'OSDBuilder -SetPath D:\OSDBuilder' -ForegroundColor Cyan
 
-        Write-Host 'Create OSDBuilder Directory Structure:                  ' -NoNewline
+        Write-Host 'Create OSDBuilder Directory Structure:              ' -NoNewline
         Write-Host 'OSDBuilder -CreatePaths' -ForegroundColor Cyan
 
-        Write-Host 'Update OSDBuilder Module to the latest version:         ' -NoNewline
+        Write-Host 'Update OSDBuilder Module to the latest version:     ' -NoNewline
         Write-Host 'OSDBuilder -Update' -ForegroundColor Cyan
         
         Write-Host ''
 
-        Write-Host 'Download the latest OneDriveSetup.exe (Production):     ' -NoNewline
-        Write-Host 'OSDBuilder -Download OneDrive' -ForegroundColor Green
-
-        Write-Host 'Download the latest OneDriveSetup.exe (Enterprise):     ' -NoNewline
-        Write-Host 'OSDBuilder -Download OneDriveEnterprise' -ForegroundColor Green
-
-        Write-Host 'Download missing Microsoft Updates for OSMedia:         ' -NoNewline
+        Write-Host 'Download missing Microsoft Updates for OSMedia:     ' -NoNewline
         Write-Host 'OSDBuilder -Download OSMediaUpdates' -ForegroundColor Green
 
-        Write-Host 'Download Windows 10 Feature Updates for Import:         ' -NoNewline
+        Write-Host 'Download Windows 10 Feature Updates for Import:     ' -NoNewline
         Write-Host 'OSDBuilder -Download FeatureUpdates' -ForegroundColor Green
 
+        Write-Host 'Download the latest OneDriveSetup.exe:              ' -NoNewline
+        Write-Host 'OSDBuilder -Download OneDrive' -ForegroundColor Green
+
+        Write-Host 'Download the latest OneDriveSetup.exe (Enterprise): ' -NoNewline
+        Write-Host 'OSDBuilder -Download OneDriveEnterprise' -ForegroundColor Green
         Write-Host ''
     }
     #===================================================================================================
