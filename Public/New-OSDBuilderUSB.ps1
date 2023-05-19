7faa384220aa8623eeedf64e46860ced8c24b866
Dakota Lewis <85767340+TitanDeploy@users.noreply.github.com>
2023-05-19 18:42:42 +0000

    Started adding Split and SplitSize parameters.

--- Public/New-OSDBuilderUSB.ps1
+++ Public/New-OSDBuilderUSB.ps1
@@ -13,6 +13,13 @@ Full Path of the OSDBuilder Media
 
 .PARAMETER USBLabel
 Label for the USB Drive
+
+.PARAMETER Split
+Split install.wim into multiple .swm files. This will be automatically set to $true if the file is reported to be over 4GB.
+
+.PARAMETER SplitSize
+The desired size of the .swm files created when using the "Split" parameter. Note: The integer value should be in MB (ie. 4000 is 4GB)
+
 #>
 function New-OSDBuilderUSB {
     [CmdletBinding()]
@@ -21,8 +28,33 @@ function New-OSDBuilderUSB {
         [string]$FullName,
         
         [ValidateLength(1,11)]
-        [string]$USBLabel
+        [string]$USBLabel,
+
+        [Parameter]
+        [Switch]$Split
     )
+
+    dynamicparam
+    {
+      if ($Split -eq $true)
+      {
+        $parameterAttribute = [System.Management.Automation.ParameterAttribute]@{
+            ParameterSetName = "__AllParameterSets"
+            Mandatory = $true
+        }
+  
+        $attributeCollection = [System.Collections.ObjectModel.Collection[System.Attribute]]::new()
+        $attributeCollection.Add($parameterAttribute)
+  
+        $dynParam1 = [System.Management.Automation.RuntimeDefinedParameter]::new(
+          'SplitSize', [Int32], $attributeCollection
+        )
+  
+        $paramDictionary = [System.Management.Automation.RuntimeDefinedParameterDictionary]::new()
+        $paramDictionary.Add('SplitSize', $dynParam1)
+        return $paramDictionary
+      }
+    }
 
     Begin {
         #=================================================
@@ -47,6 +79,8 @@ function New-OSDBuilderUSB {
 
         $AllMyOSDBMedia = @()
         $AllMyOSDBMedia = [array]$AllMyOSMedia + [array]$AllMyOSBuilds + [array]$AllMyPEBuilds
+
+        # TODO: Create variable with size of the install.wim
     }
 
     Process {
@@ -55,6 +89,8 @@ function New-OSDBuilderUSB {
 
         Write-Warning "USB will be formatted FAT32"
         Write-Warning "Install.wim larger than 4GB will FAIL"
+
+        # TODO: Create a check if install.wim is greater than 4GB or the "split" parameter is selected, then split the wim file into multiple .swm files of the desired size (4GB exactly, if not using the split and splitsize parameters).
 
         #=================================================
         Write-Verbose '19.1.14 Select Source OSMedia'
