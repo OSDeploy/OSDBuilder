9b0c9349e3ade12b2bc64bb22d6f2b12cfd5e9d2
Josh Andrae <jandrae@investigator.sa.edu.au>
2023-11-01 16:00:05 +1030

    Updated warning to include support for 23H2

--- Public/Import-OSMedia.ps1
+++ Public/Import-OSMedia.ps1
@@ -207,7 +207,7 @@ function Import-OSMedia {
         Show-ActionTime; Write-Host 'Get-OSDBuilder: Validating OSDBuilder Content'
         Show-ActionTime; Write-Warning 'This version of OSDBuilder only supports:'
         Show-ActionTime; Write-Warning 'Windows 10 1607 - 21H2'
-        Show-ActionTime; Write-Warning 'Windows 11 21H2 - 22H2'
+        Show-ActionTime; Write-Warning 'Windows 11 21H2 - 23H2'
         Show-ActionTime; Write-Warning 'Windows Server 2016 1607 - Windows Server 2022 21H1'
         Get-OSDBuilder -CreatePaths -HideDetails
         #=================================================
